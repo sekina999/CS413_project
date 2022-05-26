0b8aa4ba17c98f319649740122ca4a5ba06a20da
sekina999 <73868108+sekina999@users.noreply.github.com>
2022-05-26 15:01:35 +0200

    Create similarvideos.js

---
+++ src/components/homepage/similarvideos.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SimilarVideo = ({ title }) => {
+	return (
+		<div className="h-40 bg-blue-200 p-2" style={{ maxWidth: "370px" }}>
+			{title}
+		</div>
+	);
+};
+
+export default SimilarVideo;
