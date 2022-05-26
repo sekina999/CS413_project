00fe1e4a18b4c50ebf0e85c5217cf88582cdf58d
sekina999 <73868108+sekina999@users.noreply.github.com>
2022-05-26 15:02:01 +0200

    Create videoinfo.js

---
+++ src/components/homepage/videoinfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const VideoInfo = ({ title }) => {
+	return (
+		<div className="w-11/12 h-20  my-5 mx-auto bg-blue-200 p-2">
+			{title}
+		</div>
+	);
+};
+
+export default VideoInfo;
