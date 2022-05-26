c662672da8a7247cc1e4710bbbdd371c0c5f54d0
sekina999 <73868108+sekina999@users.noreply.github.com>
2022-05-26 14:53:47 +0200

    Create VideoContentWrapper.js

---
+++ pages/Homepage/wrappers/VideoContentWrapper.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoContentWrapper = ({ children }) => {
+	return <div className="flex flex-col flex-1"> {children} </div>;
+};
+
+export default VideoContentWrapper;
