471e064698b21a47008c2b3be28910bf0101fecf
sekina999 <73868108+sekina999@users.noreply.github.com>
2022-05-26 15:15:13 +0200

    Delete pages/Homepage directory

--- pages/Homepage/wrappers/CommentsWrapper.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CommentsWrapper = ({ children }) => {
-	return <div className="w-full h-auto">{children}</div>;
-};
-
-export default CommentsWrappe
--- pages/Homepage/wrappers/VideoContentWrapper.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const VideoContentWrapper = ({ children }) => {
-	return <div className="flex flex-col flex-1"> {children} </div>;
-};
-
-export default VideoContentWrapper;
